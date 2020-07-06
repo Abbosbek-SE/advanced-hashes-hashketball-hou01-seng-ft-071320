34dc009c9d9c4316a1dd838c8e8417897c08cd74
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-07-06 04:45:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,81 +127,151 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored(player_name)
+library = game_hash
+  location=[]
+  teams=[]
+  library.each{|loc,team|
+      location.push(loc)
+      teams.push(team)
+  }
 
-def num_points_scored(player)
-  points_scored = 0
-  game_hash.each do |team, team_attributes|
-    team_attributes[:players].each do |attribute|
-      if attribute[:player_name] == player
-        points_scored = attribute[:points]
-      end
-    end
-  end
-  points_scored
+teams.each { |index|
+    index[:players].each {|inner_index|
+        if (inner_index[:player_name] == player_name)
+            return inner_index[:points]
+        end
+}
+}
+end
+#####
+def shoe_size(player_name)
+library = game_hash
+  location=[]
+  teams=[]
+  library.each{|loc,team|
+      location.push(loc)
+      teams.push(team)
+  }
+
+teams.each { |index|
+    index[:players].each {|inner_index|
+        if (inner_index[:player_name] == player_name)
+            return inner_index[:shoe]
+        end
+}
+}
 end
 
-def shoe_size(player)
-  s_size = 0 
-  game_hash.each do |team, team_attributes|
-    team_attributes[:players].each do |attribute|
-      if attribute[:player_name] == player
-        s_size = attribute[:shoe]
-      end
-    end
-  end
-  s_size
-end
-
+#####
 def team_colors(team_name)
-  colors = nil
-  game_hash.map do |team, team_attributes|
-    if team_attributes[:team_name] == team_name
-      colors = team_attributes[:colors]
-    end
-  end
-  colors
+library = game_hash
+  location=[]
+  teams=[]
+  library.each{|loc,team|
+      location.push(loc)
+      teams.push(team)
+  }
+
+teams.each { |index|
+        if (index[:team_name] == team_name)
+            return index[:colors]
+        end
+}
 end
 
+#####
 def team_names
-  game_hash.map do |team, team_attributes|
-    team_attributes[:team_name]
-  end
+library = game_hash
+  location=[]
+  teams=[]
+  library.each{|loc,team|
+      location.push(loc)
+      teams.push(team)
+  }
+team_name = [ ]
+teams.each { |index|
+  team_name.push(index[:team_name])
+}
+return team_name
 end
 
+#####
 def player_numbers(team_name)
-  numbers = []
-  game_hash.map do |team, team_attributes|
-    if team_attributes[:team_name] == team_name
-      team_attributes[:players].map do |player, stats|
-        numbers << player[:number]
-      end
-    end
+library = game_hash
+  location=[]
+  teams=[]
+  library.each{|loc,team|
+      location.push(loc)
+      teams.push(team)
+  }
+numbers = [ ]
+teams.each { |index|
+    index[:players].each {|inner_index|
+         if (index[:team_name] == team_name)
+          numbers.push(inner_index[:number])
   end
-  numbers
+}
+}
+return numbers
 end
+####
+def num_points_scored(player_name)
+library = game_hash
+  location=[]
+  teams=[]
+  library.each{|loc,team|
+      location.push(loc)
+      teams.push(team)
+  }
 
-def player_stats(player)
-  game_hash.each do |team, team_attributes|
-    team_attributes[:players].each do |name|
-      if name[:player_name] == player
-        # binding.pry
-        return name
-     end
-   end
- end
+teams.each { |index|
+    index[:players].each {|inner_index|
+        if (inner_index[:player_name] == player_name)
+            return inner_index[:points]
+        end
+}
+}
 end
+#####
 
+def player_stats(player_name)
+library = game_hash
+  location=[]
+  teams=[]
+  library.each{|loc,team|
+      location.push(loc)
+      teams.push(team)
+  }
+
+teams.each { |index|
+    index[:players].each {|inner_index|
+        if (inner_index[:player_name] == player_name)
+            return inner_index
+        end
+}
+}
+end
+###
 def big_shoe_rebounds
-  biggest_shoes = 0
-  rebounds = 0 
-  game_hash.each do |team, team_attributes|
-    team_attributes[:players].each do |player|
-      size = player[:shoe]
-      if size > biggest_shoes
-        biggest_shoes = size
-        rebounds = player[:rebounds]
-      end
-    end
-  end
-  rebounds
+library = game_hash
+  location=[]
+  teams=[]
+  library.each{|loc,team|
+      location.push(loc)
+      teams.push(team)
+  }
+shoes = []
+teams.each { |index|
+  index[:players].each {|inner_index|
+        shoes.push (inner_index[:shoe])
+  }
+}
+teams.each { |index|
+  index[:players].each {|inner_index|
+         if (inner_index[:shoe]==shoes.max)
+            return inner_index[:rebounds]
+        end
+    }
+}
 end
